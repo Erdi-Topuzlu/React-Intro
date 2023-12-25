fa5a1e707cbe0d0fd97298715f7e8cdb69ebd13c
Erdi Topuzlu <erditpzl@gmail.com>
2023-12-25 13:52:20 +0300

    Added ParoductCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import Counter from "./Components/Counter";
-import ToDoList from "./Components/ToDoList";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import "./App.css";
+import Homepage from "./pages/Homepage/Homepage";
+import About from "./pages/About/About";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return (
-    <>
-      <Counter />
-      <hr></hr>
-      <ToDoList />
-    </>
-  );
+	return (
+		<>
+			<Navbar />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Homepage />}></Route>
+					<Route path="/about" element={<About />}></Route>
+					<Route path="*" element={<p>Not Found</p>}></Route>
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
-
 export default App;
